1e573e3401dcfc1ed96951d62e8aa1d059e55d94
RubansGo <a.moran2017@yandex.ru>
2023-05-25 13:23:01 +0300

    Третья задача

---
+++ task003/Program.cs
@@ -0,0 +1,11 @@
+﻿int Number = 0;
+Console.WriteLine("Get number");
+Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 == 0)
+{
+Console.WriteLine("Number is even");
+}
+else
+{
+Console.WriteLine("Number is odd ");
+}
