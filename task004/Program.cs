c1fbd2eb36f86aec25d529a72696fce9a6c2932a
RubansGo <a.moran2017@yandex.ru>
2023-05-25 13:25:33 +0300

    Четвертая задача

---
+++ task004/Program.cs
@@ -0,0 +1,12 @@
+﻿int Number = 0;
+int X = 1;
+Console.WriteLine("Get number");
+Number = Convert.ToInt32(Console.ReadLine());
+while (X < Number)
+{
+if(X % 2 == 0)
+{
+Console.WriteLine(X);
+}
+X++;
+}
