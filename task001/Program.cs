0caba60b4dfcffa00552089d4a50a2484fd6f4c4
RubansGo <a.moran2017@yandex.ru>
2023-05-25 12:55:09 +0300

    Первая задача

---
+++ task001/Program.cs
@@ -0,0 +1,16 @@
+﻿int FirstNumber = 0;
+int SecondNumer = 0;
+Console.WriteLine("Get first numbers");
+FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Get second numbers");
+SecondNumer = Convert.ToInt32(Console.ReadLine());
+if (FirstNumber > SecondNumer)
+{
+    Console.WriteLine("Greater number: " + FirstNumber);
+    Console.WriteLine("Smaller number: " + SecondNumer);
+}
+else 
+{
+    Console.WriteLine("Greater number: " + SecondNumer);
+    Console.WriteLine("Smaller number: " + FirstNumber);
+}
